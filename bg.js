6c5555eb6650f1cb90eb1d5c63314a408b973b6c
Tobias H <69078401+TobiasHDeveloper@users.noreply.github.com>
2021-06-06 21:30:15 +0200

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,5 +1,6 @@
 chrome.browserAction.onClicked.addListener(
     function(tab) {
-        window.open("https://calendar.google.com/calendar/u/0/r", "", "width=10000px,height=10000");
+        //open google calendar, with an absurd width and height, to fill out the whole screen (it won't go past the max diamentions)
+        window.open("https://calendar.google.com/calendar/u/0/r", "", "width=10000px,height=10000"); //replace the url here, to open any page
     }
 );
